8df5db9d897df1656e5c789b4eb6f43ed723815f
Doha <adamdoha@linecorp.com>
2021-11-28 21:16:06 +0900

    initial project

---
+++ src/main/java/com/deepforest/dcinsideserver/DcinsideServerApplication.java
@@ -0,0 +1,13 @@
+package com.deepforest.dcinsideserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcinsideServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcinsideServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deepforest/dcinsideserver/DcinsideServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepforest.dcinsideserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcinsideServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
