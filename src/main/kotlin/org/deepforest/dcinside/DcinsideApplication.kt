f5711604dcb724e10130ae47cf7872b61e3e18c9
ParkJiwoon <bcp0109@gmail.com>
2021-12-03 00:24:08 +0900

    first commit

---
+++ src/main/kotlin/org/deepforest/dcinside/DcinsideApplication.kt
@@ -0,0 +1,11 @@
+package org.deepforest.dcinside
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DcinsideApplication
+
+fun main(args: Array<String>) {
+	runApplication<DcinsideApplication>(*args)
+}
