867895d2fcc533aadde40e2b63fa87e25a8b90b0
Doha <adamdoha@naver.com>
2021-12-20 00:39:44 +0900

    Add error field

--- support/src/main/kotlin/org/deepforest/dcinside/dto/ResponseDto.kt
+++ support/src/main/kotlin/org/deepforest/dcinside/dto/ResponseDto.kt
@@ -2,24 +2,25 @@ package org.deepforest.dcinside.dto
 
 class ResponseDto<R>(
     val success: Boolean,
-    val result: R?
+    val result: R?,
+    val error: Error?
 ) {
 
     companion object {
         fun ok(): ResponseDto<Void> {
-            return ResponseDto(true, null)
+            return ResponseDto(true, null, null)
         }
 
         fun <R> ok(result: R): ResponseDto<R> {
-            return ResponseDto(true, result)
+            return ResponseDto(true, result, null)
         }
 
-        fun fail(): ResponseDto<Void> {
-            return ResponseDto(false, null)
+        fun fail(code: String, message: String): ResponseDto<Void> {
+            return ResponseDto(false, null, Error(code, message))
         }
 
-        fun <R> fail(result: R): ResponseDto<R> {
-            return ResponseDto(false, result);
+        fun <R> fail(code: String): ResponseDto<R> {
+            return ResponseDto(false, null, Error(code, null));
         }
     }
 }
