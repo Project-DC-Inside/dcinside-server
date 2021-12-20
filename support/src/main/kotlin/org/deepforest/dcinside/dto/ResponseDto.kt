d6ac242d8b45bd3ae4bcd153aeffaa957db8ed0e
DolphaGo <adamdoha@naver.com>
2021-12-20 10:06:03 +0900

    공통 ResponseDto 적용 (#6)
    * Set ResponseDto
    
    * Remove unused properties
    
    * Add error field
    
    * Edit void type about fail response
    
    * apply review

---
+++ support/src/main/kotlin/org/deepforest/dcinside/dto/ResponseDto.kt
@@ -0,0 +1,22 @@
+package org.deepforest.dcinside.dto
+
+class ResponseDto<R>(
+    val success: Boolean,
+    val result: R? = null,
+    val error: Error? = null
+) {
+
+    companion object {
+        fun ok(): ResponseDto<Void> {
+            return ResponseDto(true)
+        }
+
+        fun <R> ok(result: R): ResponseDto<R> {
+            return ResponseDto(true, result)
+        }
+
+        fun fail(code: String, message: String? = null): ResponseDto<Void> {
+            return ResponseDto(false, error = Error(code, message))
+        }
+    }
+}
