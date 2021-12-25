1790cf3868342ea2de64a4ae63a7110a1ea0304a
Doha <adamdoha@naver.com>
2021-12-25 22:40:46 +0900

    fix: EnableJpaAuditing 추가

--- api/src/main/kotlin/org/deepforest/dcinside/DcinsideApplication.kt
+++ api/src/main/kotlin/org/deepforest/dcinside/DcinsideApplication.kt
@@ -2,7 +2,9 @@ package org.deepforest.dcinside
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 class DcinsideApplication
 
