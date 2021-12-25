906419dc8b450e85753f65e7f6b7f6544df833f9
DolphaGo <adamdoha@naver.com>
2021-12-25 23:00:58 +0900

    feat: Post, Comment, Gallery, BaseEntity 설계 (#11)
    * feat: Post, Comment, Gallery, BaseEntity 설계
    
    * fix: EnableJpaAuditing 추가

---
+++ core/src/main/kotlin/org/deepforest/dcinside/entity/BaseEntity.kt
@@ -0,0 +1,21 @@
+package org.deepforest.dcinside.entity
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.EntityListeners
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+class BaseEntity {
+    @CreatedDate
+    @Column(name = "createdAt", columnDefinition = "timestamp")
+    var createdAt: LocalDateTime = LocalDateTime.MIN
+
+    @LastModifiedDate
+    @Column(name = "updatedAt", columnDefinition = "timestamp")
+    var updatedAt: LocalDateTime = LocalDateTime.MIN
+}
---
+++ core/src/main/kotlin/org/deepforest/dcinside/entity/post/PostStatistics.kt
@@ -0,0 +1,34 @@
+package org.deepforest.dcinside.entity.post
+
+import javax.persistence.*
+
+@Table(
+    name = "post_statistics", indexes = [
+        Index(columnList = "viewCount"),
+        Index(columnList = "likeCount"),
+        Index(columnList = "commentCount"),
+    ]
+)
+@Entity
+class PostStatistics(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_statistics_id")
+    val id: Long = 0L,
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    val post: Post,
+
+    @JoinColumn(name = "view_count")
+    var viewCount: Long = 0L,
+
+    @JoinColumn(name = "like_count")
+    var likeCount: Long = 0L,
+
+    @JoinColumn(name = "dislike_count")
+    var dislikeCount: Long = 0L,
+
+    @JoinColumn(name = "comment_count")
+    var commentCount: Long = 0L
+)
