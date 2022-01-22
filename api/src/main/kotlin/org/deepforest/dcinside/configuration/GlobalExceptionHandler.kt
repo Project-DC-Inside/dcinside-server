2709bbbf8ea27062b98dc59b7e71c9198cd1a105
ParkJiwoon <bcp0109@gmail.com>
2022-01-22 20:34:58 +0900

    feat: Exception 핸들링 추가

--- api/src/main/kotlin/org/deepforest/dcinside/configuration/GlobalExceptionHandler.kt
+++ api/src/main/kotlin/org/deepforest/dcinside/configuration/GlobalExceptionHandler.kt
@@ -3,6 +3,7 @@ package org.deepforest.dcinside.configuration
 import org.deepforest.dcinside.dto.ErrorCode
 import org.deepforest.dcinside.dto.ResponseDto
 import org.deepforest.dcinside.exception.ApiException
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -22,5 +23,23 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
     protected fun handleBadCredentialsException(e: BadCredentialsException): ResponseDto<Unit> {
         logger.error("message", e)
         return ResponseDto.fail(ErrorCode.MISMATCH_PASSWORD)
+    }
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    protected fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseDto<Unit> {
+        logger.error("message", e)
+        return ResponseDto.fail(ErrorCode.CONFLICT_GALLERY, e.message)
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException::class)
+    protected fun handleDataIntegrityViolationException(e: DataIntegrityViolationException): ResponseDto<Unit> {
+        logger.error("message", e)
+        return ResponseDto.fail(ErrorCode.CONFLICT)
+    }
+
+    @ExceptionHandler(Exception::class)
+    protected fun handleException(e: Exception): ResponseDto<Unit> {
+        logger.error("message", e)
+        return ResponseDto.fail(ErrorCode.INTERNAL_SERVER_ERROR)
     }
 }
